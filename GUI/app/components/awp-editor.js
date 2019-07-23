a0b7d7d4bce4740998c5f4ce08d749176004ca96
fma <smalltibbers@gmail.com>
2019-07-23 19:36:15 +0300

    now we can edit modules, workplaces and nodes

--- GUI/app/components/awp-editor.js
+++ GUI/app/components/awp-editor.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['nodes-block'],
+  classNames: ['awp-editor'],
   actions: {
     saveAwp: function() {
       this.awp.save();
--- GUI/app/components/block-editor.js
+++ GUI/app/components/block-editor.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['nodes-block'],
+  classNames: ['block-editor'],
   actions: {
     saveBlock: function() {
       this.block.save();
