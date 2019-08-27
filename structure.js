7c0638829dc72671a301cad716a582ab0eb2e65c
micael <micaelalastor@gmail.com>
2019-08-27 17:03:11 +0300

    class based logic works somehow now

--- structure.js
+++ structure.js
@@ -1,6 +1,83 @@
 const uuidv4 = require('uuid/v4');
 
-module.exports.Network = class LocalNetwork {
+//node is either single pc or projector
+let Node = module.exports.Node = class Node {
+    constructor(nodeData) {
+        this.block = nodeData.block;
+        this.awp = nodeData.awp;
+
+        this.name = nodeData.name;
+        this.ip = nodeData.ip;
+        this.mac = nodeData.mac;
+        this.powerstate = nodeData.powerstate;
+        this.nodestype = nodeData.nodestype;
+        //if we create data from json so id is already there
+        if(nodeData.id) {
+            this.id = nodeData.id;
+        }
+        //if we create new node for client request
+        else {
+            this.id = uuidv4();
+        }
+    }
+};
+
+//automated workplace (either set of pc or projectors)
+let Awp = module.exports.Awp = class Awp {
+    constructor(awpData) {
+        this.nodes = [];
+        this.name = awpData.name;
+        this.block = awpData.block;
+        this.nodestype = awpData.nodestype;
+        //if we create data from json so id is already there and awp has its nodes
+        if(awpData.id){
+            let self = this;
+            this.id = awpData.id;
+            awpData.nodes.forEach(function(nodeData){
+                let newNode = new Node(nodeData);
+                self.pushNode(newNode);
+            })
+        }
+        //if we create new awp for client request
+        else {
+            this.id = uuidv4();
+        }
+    }
+
+    pushNode(node) {
+        node.nodestype = this.nodestype;
+        this.nodes.push(node);
+    }
+};
+
+//block of workplaces
+let Block = module.exports.Block = class Block {
+    constructor(blockData) {
+        this.awps = [];
+        this.name = blockData.name;
+
+        //if we create data from json so id is already there and block has workplaces
+        if(blockData.id){
+            let self = this;
+            this.id = blockData.id;
+            blockData.awps.forEach(function(awpData){
+                let newAwp = new Awp(awpData);
+                self.pushAwp(newAwp);
+            })
+        }
+        //if we create new block for client request
+        else {
+            this.id = uuidv4();
+        }
+    }
+
+    pushAwp(awp) {
+        this.awps.push(awp);
+    }
+};
+
+
+let Network = module.exports.Network = class LocalNetwork {
     constructor() {
         this.blocks = [];
     }
@@ -34,80 +111,5 @@ module.exports.Network = class LocalNetwork {
         return block.awps.find(function (awp) {
             return awp.id === awp_id;
         });
-    }
-};
-
-//block of workplaces
-module.exports.Block = class Block {
-    constructor(blockData) {
-        this.awps = [];
-        this.name = blockData.name;
-
-        //if we create data from json so id is already there and block has workplaces
-        if(blockData.id){
-            let self = this;
-            this.id = blockData.id;
-            blockData.awps.forEach(function(awpData){
-                let newAwp = new Awp(awpData);
-                self.pushAwp(newAwp);
-            })
-        }
-        //if we create new block for client request
-        else {
-            this.id = uuidv4();
-        }
-    }
-
-    pushAwp(awp) {
-        this.awps.push(awp);
-    }
-};
-
-//automated workplace (either set of pc or projectors)
-module.exports.Awp = class Awp {
-    constructor(awpData) {
-        this.nodes = [];
-        this.name = awpData.name;
-        this.block = awpData.block;
-        this.nodestype = awpData.nodestype;
-        //if we create data from json so id is already there and awp has its nodes
-        if(awpData.id){
-            let self = this;
-            this.id = awpData.id;
-            awpData.nodes.forEach(function(nodeData){
-                let newNode = new Node(nodeData);
-                self.pushNode(newNode);
-            })
-        }
-        //if we create new awp for client request
-        else {
-            this.id = uuidv4();
-        }
-    }
-
-    pushNode(node) {
-        node.nodestype = this.nodestype;
-        this.nodes.push(node);
-    }
-};
-
-module.exports.Node = class Node {
-    constructor(nodeData) {
-        this.block = nodeData.block;
-        this.awp = nodeData.awp;
-
-        this.name = nodeData.name;
-        this.ip = nodeData.ip;
-        this.mac = nodeData.mac;
-        this.powerstate = nodeData.powerstate;
-        this.nodestype = nodeData.nodestype;
-        //if we create data from json so id is already there
-        if(nodeData.id) {
-            this.id = nodeData.id;
-        }
-        //if we create new node for client request
-        else {
-            this.id = uuidv4();
-        }
     }
 };
